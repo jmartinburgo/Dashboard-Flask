35f821a785de599c25ee7848a9b74db152d7e491
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-08-27 17:24:57 -0300

    First commit

---
+++ application/routes.py
@@ -0,0 +1,29 @@
+from application import app
+from flask import render_template, url_for
+import pandas as pd 
+import json
+import plotly
+import plotly_express as px
+
+
+@app.route('/')
+def index():
+    #Grafico 1 
+    df = px.data.medals_wide()
+    fig1= px.bar(df,x="nation",y=['gold','silver','bronze'],title="Wide=Form Input")  
+
+    graph1JSON= json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph two
+    df = px.data.iris()
+    fig2 = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species',  title="Iris Dataset")
+    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph three
+    df = px.data.gapminder().query("continent=='Oceania'")
+    fig3 = px.line(df, x="year", y="lifeExp", color='country',  title="Life Expectancy")
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html',graph1JSON=graph1JSON,graph2JSON=graph2JSON,graph3JSON=graph3JSON)
+
